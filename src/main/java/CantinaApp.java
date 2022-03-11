ea3dfc3ce0b6068f6c7561de39106f71e79a6903
Giovanni <gvn.gentile@gmail.com>
2022-03-11 09:34:05 -0300

    chore: add TODO's

--- src/main/java/CantinaApp.java
+++ src/main/java/CantinaApp.java
@@ -2,6 +2,9 @@ import utils.MenuPrincipal;
 
 public class CantinaApp {
 
+    // TODO: Tratamento InputMismatchException
+    // TODO: Lógica para reiniciar programa após 3 tentativas mal sucedidas de pedido
+
     public static void main(String[] args) {
         MenuPrincipal.montarMenuPrincipal();
     }
